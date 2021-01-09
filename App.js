3e73b37bed67760f74039c159e6870e4253d85e0
joshua-montanari <joshua.montanari@gmail.com>
2021-01-09 12:10:19 -0500

    Finished home page for clone

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './Components/CarItem/index'
+import { StyleSheet, View } from 'react-native';
+import CarsList from './Components/CarsList/index'
+import Header from './Components/Header/index'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem 
-        name={'Model X'} 
-        tagline={'Order Online For'} 
-        taglineCTA={'Touchless Delivery'}
-        image={require('./assets/images/ModelX.jpeg')}  
-      />
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
